8d1d9a1b4a2bdc86a6920aaebb4e8a6472b5e669
unknown <rupak2014.cmoshmc@gmail.com>
2025-04-29 11:56:32 +0600

    class 1 dt.28-04-2025

---
+++ JS Class and HW-1/js/script.js
@@ -0,0 +1,74 @@
+
+
+let price_per_kg = 300;
+let quantity = 6;
+total = price_per_kg * quantity;
+disPrice = total-total*0.10;
+
+console.log(total);
+console.log(disPrice);
+
+
+
+if (total>=1000) {
+    console.log(`Congratulations! you have received 10% discount on your purchase price (Tk.${total}). you are requested to pay the discounted price of Tk.${disPrice}/- only.`);
+} else{
+    console.log(`Thank you for purchasing products from our shop. You are requested to pay Tk.${total}/- only as product cost.`)
+}
+
+
+// Revised product quantity 
+
+quantity = 3;
+total = price_per_kg * quantity;
+disPrice = total-total*0.10;
+
+console.log(total);
+console.log(disPrice);
+
+
+if (total>=1000) {
+    console.log(`Congratulations! you have received 10% discount on your purchase price (Tk.${total}). you are requested to pay the discounted price of Tk.${disPrice}/- only.`);
+} else{
+    console.log(`Thank you for purchasing products from our shop. You are requested to pay Tk.${total}/- only as product cost.`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 5;
+// let b = 7;
+// let c = a + b;
+// console.log(c);
+
+// let color;
+// let mode;
+// if (mode === "dark-mode") {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+// console.log(color);
+
+// let age = 15;
+// if (age <18){
+//     console.log ("Junior");
+// } else if (age < 60) {
+//     console.log ("Adult");
+// } else{
+//     console.log ("Senior")
+// }
+
+
+// let userName = prompt("Write your name without space:");
+// let str = userName;
+// console.log(userName);
+// console.log("@" + userName + str.length);
