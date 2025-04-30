d445b08412f0965d770418e4d6d935249a60983b
Tawqi <tawqitahmedchy@proton.me>
2025-04-30 20:26:59 +0600

    Class-2

---
+++ JS Class and HW-2/js/code.js
@@ -0,0 +1,57 @@
+// let age = prompt("What is you are?");
+
+// if (age>=60){
+//     console.log(`Your are ${age} years old and you are an old man`)
+// } else if (age>=45) {
+//     console.log(`Your are ${age} years old and you are an aged person`)
+// } else if(age>=30){
+//     console.log(`Your are ${age} years old and you are a active person`)
+// } else if(age>=18){
+//     console.log(`Your are ${age} years old and you are adult person`)
+// } else{
+//     console.log(`Your are ${age} years old and you are not adult person`)
+// }
+
+
+
+
+// let mark = prompt("Write your mark obtained");
+
+// if (mark>=80){
+//     console.log(`Mark : ${mark} and your GPA is 5.0 `)
+// } else if (mark>=60) {
+//     console.log(`Mark : ${mark} and your GPA is 4.0 `)
+// } else if(mark>=45){
+//     console.log(`Mark : ${mark} and your GPA is 3.0`)
+// } else if(mark>=40){
+//     console.log(`Mark : ${mark} and your GPA is 2.0`)
+// } else if(mark>=33){
+//     console.log(`Mark : ${mark} and your GPA is 1.0`)
+// } else{
+//     console.log(`Mark : ${mark} and your GPA is 0`)
+// }
+
+// let userName = "user";
+// let password = "open";
+
+
+// if (userName == "admin" && password == "open"){
+//     console.log(`Successfully logged in. Welcome ${userName}.`)
+// } else if(userName == "user" && password == "open"){
+//     console.log(`Successfully logged in. Welcome ${userName}.`)
+// } else {
+//     console.log("User name & password is not correct; type valide user name & password.")
+// }
+
+
+// let userName = "admin";
+// let password = "open";
+
+
+// if (userName == "admin" && password == "open" || userName == "user" && password == "open" || userName=="guest" && password==1234){
+//     console.log(`Successfully logged in. Welcome ${userName}.`)
+// } else {
+//     console.log("User name & password is not correct; type valide user name & password.")
+// }
+
+
