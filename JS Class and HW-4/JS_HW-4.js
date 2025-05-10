0b79dfc6198e83c4b3af9cb9c9d094886765a12f
unknown <rupak2014.cmoshmc@gmail.com>
2025-05-10 17:26:24 +0600

    HW-4 Update

--- JS Class and HW-4/JS_HW-4.js
+++ JS Class and HW-4/JS_HW-4.js
@@ -1,13 +1,13 @@
-// Problem - 1
-// Find the area of ​​a triangle where the values ​​of the base and height is given.
-function Triangle_area(base, height){
-    T_area = (base *  height)/2;
-    document.writeln(`If the base of a Triangle is ${base} and height is ${height} than the area of that triangle is ${T_area}`)
-}
-let b = prompt('Enter the mesurement of Base');
-let h = prompt('Enter the mesurement of Height');
+// // Problem - 1
+// // Find the area of ​​a triangle where the values ​​of the base and height is given.
+// function Triangle_area(base, height){
+//     T_area = (base *  height)/2;
+//     document.writeln(`If the base of a Triangle is ${base} and height is ${height} than the area of that triangle is ${T_area}`)
+// }
+// let b = prompt('Enter the mesurement of Base');
+// let h = prompt('Enter the mesurement of Height');
 
-Triangle_area(b, h);
+// Triangle_area(b, h);
 
 // Problem - 2
 // Create a function that can calcul income tax of an Employee. 
@@ -20,7 +20,7 @@ function income_tax(salary, tax_rate){
     yearly_salary = salary * 12;
     annual_income_tax = yearly_salary * tax_rate;
 
-    document.writeln(`Yearly Salary of the Employee is ${yearly_salary} and according to Govt. rules, this is a taxable income and he have to pay ${tax_rate}% tax from his annual income`);
+    document.writeln(`Yearly Salary of the Employee is ${yearly_salary} and according to Govt. rules, this is a taxable income and he have to pay ${tax_rate*100}% tax from his annual income`);
     document.writeln('<br>')
     document.writeln(`Finally, The Employee have to pay ${annual_income_tax} annually as income tax`); 
     }
